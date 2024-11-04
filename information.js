eb82b112461e58eecd85680c19c8122f4c402703
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-11-04 19:03:40 +0800

    initial commit

---
+++ information.js
@@ -0,0 +1,11 @@
+let firstName = "Aaron Raymund";
+let middleName = "Rivera";
+let lastName = "Paguirigan";
+let age = 19;
+let favoriteNumber = 12;
+let favoriteColor = "Blue";
+
+console.log("Full Name: " + firstName + " " + middleName + " " + lastName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
